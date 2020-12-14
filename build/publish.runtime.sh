fa104cf9cd88dd0b8ed924426dac21e2b74ffd4a
Pablo Ordoñez <pablo@miracledevs.com>
2020-12-14 14:59:54 -0300

    Updated publish scripts.

--- build/publish.runtime.sh
+++ build/publish.runtime.sh
@@ -27,7 +27,7 @@ block "$index - PUBLISH AND ZIP $tool FOR $C_CYAN $runtime $C_TRANSPARENT"
 
 execute "rm -rf $deployDir/$runtime/"
 
-execute "dotnet publish $toolProject -c Release -f net5.0 -r $runtime -o ../$deployDir/$runtime/ -v q"
+execute "dotnet publish $toolProject -c Release -f net5.0 -r $runtime -o $deployDir/$runtime/ -v q"
 
 execute "rm -rf $distDir/$zipFile"
 
