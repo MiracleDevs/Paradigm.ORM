c016dcbfb4c1baa4abd1ccdeb41ffbf4e431a1bc
Pablo Ordoñez <pablo@miracledevs.com>
2020-12-14 13:18:26 -0300

    Updated vb tests.

--- src/Paradigm.ORM.Vb.Tests/Tests/MySql/StoredProcedureTest.vb
+++ src/Paradigm.ORM.Vb.Tests/Tests/MySql/StoredProcedureTest.vb
@@ -14,7 +14,7 @@ Namespace Tests.MySql
         <TestInitialize()>
         Public Sub Initialize()
 
-            Connector = New MySqlDatabaseConnector("Server=localhost;Database=Test;User=test;Password=Paradigm_Test_1234;Connection Timeout=3600")
+            Connector = New MySqlDatabaseConnector("Server=localhost;Database=test;User=root;Password=Paradigm_Test_1234;Connection Timeout=3600")
             Connector.Open()
 
             Connector.ExecuteNonQuery("
